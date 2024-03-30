8d48e37f414032d857556b8e77fafce615dc48b2
Lyapunov Alexander <lyapunovrabota@gmail.com>
2024-03-30 17:08:22 +0300

    Обновил ридмик. Убрал лишние отступы зачем то

--- src/main/java/discordBot/App.java
+++ src/main/java/discordBot/App.java
@@ -8,27 +8,20 @@ import discord4j.core.object.entity.channel.MessageChannel;
 
 public class App {
     public static void main(String[] args) {
-
         String token = System.getenv("BOT_TOKEN");
         DiscordClient client = DiscordClient.create(token);
         GatewayDiscordClient gateway = client.login().block();
 
         assert gateway != null;
-        gateway.on(MessageCreateEvent.class).
+        gateway.on(MessageCreateEvent.class).subscribe(event ->
+        {
+            Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                MessageChannel channel = message.getChannel().block();
+                channel.createMessage("pong").block();
+            }
+        });
 
-                subscribe(event ->
-
-                {
-                    Message message = event.getMessage();
-                    if ("!ping".equals(message.getContent())) {
-                        MessageChannel channel = message.getChannel().block();
-                        channel.createMessage("Pong!").block();
-                    }
-                });
-
-        gateway.onDisconnect().
-
-                block();
-
+        gateway.onDisconnect().block();
     }
 }
